9b102cdc3a94eed6786ddbec5e7139fc3a909c82
Slava Losev <lossev27@yandex.ru>
2021-09-05 16:56:44 +0300

    исправил замечания ревью

--- script/index.js
+++ script/index.js
@@ -1,5 +1,6 @@
 let btnProfilEdit = document.querySelector('.profil__edit');
 let btnPopupClose = document.querySelector('.popup__close')
+let btnSubmitForm = document.querySelector('.form__submit-button');
 let popup = document.querySelector('.popup');
 let nameInput = document.querySelector('.form__input_type_name');
 let jobInput = document.querySelector('.form__input_type_job');
@@ -7,21 +8,21 @@ let form = document.querySelector('.form');
 let profilName = document.querySelector('.profil__name');
 let profilJob = document.querySelector('.profil__job');
 
-function openPopup (event) {
- event.preventDefault();
-if (!popup.classList.contains('popup__open')) {
+function tooglePopup () {
+if (!popup.classList.contains('popup_open')) {
   nameInput.value = profilName.textContent;
   jobInput.value = profilJob.textContent;
 }
+  popup.classList.toggle('popup_open');
+}
+
+function formSubmitHandler (event) {
+  event.preventDefault();
   profilName.textContent = nameInput.value;
   profilJob.textContent = jobInput.value;
-  popup.classList.toggle('popup__open');
+  tooglePopup();
 }
 
-function closePopup () {
-  popup.classList.remove('popup__open')
-}
-
-btnProfilEdit.addEventListener('click', openPopup);
-btnPopupClose.addEventListener('click', closePopup);
-form.addEventListener('submit', openPopup);
+btnProfilEdit.addEventListener('click', tooglePopup);
+btnPopupClose.addEventListener('click', tooglePopup);
+form.addEventListener('submit', formSubmitHandler);
